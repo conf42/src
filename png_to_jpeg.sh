efb45dffcc11fb32af4c4d4c4575b54cfb6326e2
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2021-03-05 08:48:35 +0000

    Testing

--- png_to_jpeg.sh
+++ png_to_jpeg.sh
@@ -7,7 +7,6 @@ if [ "$(uname)" == "Darwin" ]; then
     find ./docs/assets/podcasts -iname "*.png" -type f -exec sh -c 'sips -s format jpeg "$0" --out "${0%.png}.jpeg"' {} \;
 # linux (imagemagic)
 else
-    sudo apt-get install -y imagemagick
     find ./docs/assets/headshots -iname "*.png" -type f -exec sh -c 'convert "$0" -quality 90 "${0%.png}.jpeg"' {} \;
     find ./docs/assets/splash -iname "*.png" -type f -exec sh -c 'convert "$0" -quality 90 "${0%.png}.jpeg"' {} \;
     find ./docs/assets/podcasts -iname "*.png" -type f -exec sh -c 'convert "$0" -quality 90 "${0%.png}.jpeg"' {} \;
