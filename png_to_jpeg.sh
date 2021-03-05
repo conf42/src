a5639b83a03228386cda09e5e59d870e0b687cb6
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2021-03-05 08:40:03 +0000

    Unnecessary

--- png_to_jpeg.sh
+++ png_to_jpeg.sh
@@ -6,7 +6,7 @@ if [ "$(uname)" == "Darwin" ]; then
     find ./docs/assets/splash -iname "*.png" -type f -exec sh -c 'sips -s format jpeg "$0" --out "${0%.png}.jpeg"' {} \;
     find ./docs/assets/podcasts -iname "*.png" -type f -exec sh -c 'sips -s format jpeg "$0" --out "${0%.png}.jpeg"' {} \;
 # linux (imagemagic)
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+else
     sudo apt-get install -y imagemagick
     find ./docs/assets/headshots -iname "*.png" -type f -exec sh -c 'convert "$0" -quality 90 "${0%.png}.jpeg"' {} \;
     find ./docs/assets/splash -iname "*.png" -type f -exec sh -c 'convert "$0" -quality 90 "${0%.png}.jpeg"' {} \;
