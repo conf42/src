85da2698176264e0136ae06bbc604b5cebf70510
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2021-05-03 13:51:20 +0100

    Generate the secret subpage

--- generate.py
+++ generate.py
@@ -159,11 +159,23 @@ for event in events:
         with open(BASE_FOLDER + "/" + talk.get("short_url").replace(".html","")  + ".html", "w") as f:
             template = env.get_template("talk.html")
             f.write(template.render(event=event, talk=talk, **context))
+        if event.get("secret_url"):
+            with open(BASE_FOLDER + "/" + event.get("secret_url") + "_" + talk.get("short_url").replace(".html","")  + ".html", "w") as f:
+                template = env.get_template("talk.html")
+                event_copy = event.copy()
+                event_copy["reveal_videos"] = True
+                f.write(template.render(event=event_copy, talk=talk, **context))
 
     # template the main event page
     with open(BASE_FOLDER + "/" + event.get("short_url").replace(".html","") + ".html", "w") as f:
         template = env.get_template("event.html")
         f.write(template.render(event=event, **context))
+
+    # template the secret event page
+    if event.get("secret_url"):
+        with open(BASE_FOLDER + "/" + event.get("secret_url") + ".html", "w") as f:
+            template = env.get_template("event.html")
+            f.write(template.render(event=event, prefix=event.get("secret_url")+"_", **context))
 
 # generate the static bits
 for page in ["index.html", "podcast.html", "sponsor.html", "code-of-conduct.html"]:
