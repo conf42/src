6d808c001dcc4c7427fa60fea64401400be1927c
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2021-06-25 09:35:27 +0100

    Template out the toc

--- generate.py
+++ generate.py
@@ -230,7 +230,7 @@ for podcast in podcasts:
         urls.append((podcast.get("short_url").replace(".html",""), 0.81))
 
 # generate the static bits
-for page in ["index.html", "podcast.html", "sponsor.html", "code-of-conduct.html"]:
+for page in ["index.html", "podcast.html", "sponsor.html", "code-of-conduct.html", "terms-and-conditions.html"]:
     with open(BASE_FOLDER + "/" + page, "w") as f:
         template = env.get_template(page)
         f.write(template.render(page=page, **context))
