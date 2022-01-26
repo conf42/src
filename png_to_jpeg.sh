44748f071723952e40ed8dbb316ead6e78c7c98b
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2022-01-26 21:10:51 +0000

    When prepping the images, scale them down to 500px

--- png_to_jpeg.sh
+++ png_to_jpeg.sh
@@ -2,12 +2,12 @@
 
 # mac (sips built-in)
 if [ "$(uname)" == "Darwin" ]; then
-    find ./docs/assets/headshots -iname "*.png" -type f -exec sh -c 'sips -s format jpeg "$0" --out "${0%.png}.jpg"' {} \;
-    find ./docs/assets/splash -iname "*.png" -type f -exec sh -c 'sips -s format jpeg "$0" --out "${0%.png}.jpg"' {} \;
-    find ./docs/assets/podcasts -iname "*.png" -type f -exec sh -c 'sips -s format jpeg "$0" --out "${0%.png}.jpg"' {} \;
+    find ./docs/assets/headshots -iname "*.png" -type f -exec sh -c 'sips -Z 500 -s format jpeg "$0" --out "${0%.png}.jpg"' {} \;
+    find ./docs/assets/splash -iname "*.png" -type f -exec sh -c 'sips -Z 500 -s format jpeg "$0" --out "${0%.png}.jpg"' {} \;
+    find ./docs/assets/podcasts -iname "*.png" -type f -exec sh -c 'sips -Z 500 -s format jpeg "$0" --out "${0%.png}.jpg"' {} \;
 # linux (imagemagick)
 else
-    find ./docs/assets/headshots -iname "*.png" -type f | xargs --verbose -n 1 bash -c 'convert "$0" "${0%.*}.jpg"'
-    find ./docs/assets/splash -iname "*.png" -type f | xargs --verbose -n 1 bash -c 'convert "$0" "${0%.*}.jpg"'
-    find ./docs/assets/podcasts -iname "*.png" -type f | xargs --verbose -n 1 bash -c 'convert "$0" "${0%.*}.jpg"'
+    find ./docs/assets/headshots -iname "*.png" -type f | xargs --verbose -n 1 bash -c 'convert "$0" -resize 500 "${0%.*}.jpg"'
+    find ./docs/assets/splash -iname "*.png" -type f | xargs --verbose -n 1 bash -c 'convert "$0" -resize 500 "${0%.*}.jpg"'
+    find ./docs/assets/podcasts -iname "*.png" -type f | xargs --verbose -n 1 bash -c 'convert "$0" -resize 500 "${0%.*}.jpg"'
 fi
