59fd99d914378e75da5f7cb10c5825c92d0c8674
Mikolaj Pawlikowski <mikolaj@pawlikowski.pl>
2023-11-15 20:50:57 +0000

    Generate the hall of fame page

--- generate.py
+++ generate.py
@@ -505,7 +505,7 @@ for post in posts:
 # MAIN PAGES
 print(DIVIDER)
 print("Generating main pages")
-for page in ["index.html", "podcast.html", "sponsor.html", "sponsorship.html", "code-of-conduct.html", "blog.html", "seradio.html"]:
+for page in ["index.html", "podcast.html", "sponsor.html", "sponsorship.html", "code-of-conduct.html", "blog.html", "seradio.html", "hall-of-fame.html"]:
     with open(BASE_FOLDER + "/" + page, "w") as f:
         print("Writing out", page)
         template = env.get_template(page)
